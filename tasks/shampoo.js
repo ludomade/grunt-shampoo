f718d2811b2c407bd1b967b6b0b191ceac76bf53
Mike Chang <chang@soapcreative.com>
2014-07-24 23:47:54 -0700

    jshint. general cleanup and updated messages.

--- tasks/shampoo.js
+++ tasks/shampoo.js
@@ -19,46 +19,42 @@ var request = require("request"),
 
 var client = null;
 
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
+
   grunt.registerMultiTask( "shampoo", "Retrieve content from the Shampoo CMS API on shampoo.io.", function() {
 
     var _ = grunt.util._;
 
-    function makeClient(options) {
-        
-        return knox.createClient(_.pick(options, [
+    function makeClient( options ) {
+      return knox.createClient( _.pick(options, [
         'region', 'endpoint', 'port', 'key', 'secret', 'access', 'bucket', 'secure', 'headers', 'style'
-        ]));
+      ]));
+    }
 
-    };
-
-
-    function getMediaAssets( obj, collection, mediaOut ){
+    function getMediaAssets( obj, collection, mediaOut ) {
 
       for( var key in obj ) {
 
-        if( typeof obj[key] === "object" ){
+        if( typeof obj[key] === "object" ) {
               
           getMediaAssets( obj[key], collection, mediaOut );
 
-        } else if(typeof obj[key] == "string") {
+        } else if( typeof obj[key] === "string" ) {
 
-            if(obj[key].indexOf(".amazonaws.com/") >= 0 ) {
-                
-                var dest = obj[key].replace("http://", "").replace("https://", "");
-                dest = dest.split("/");
-                dest.shift();
-                dest = dest.join("/");
+          if(obj[key].indexOf(".amazonaws.com/") >= 0 ) {
+              
+              var dest = obj[key].replace("http://", "").replace("https://", "");
+              dest = dest.split("/");
+              dest.shift();
+              dest = dest.join("/");
 
-                if(collection.indexOf(dest) < 0) {
-                  collection.push(dest);
-                }
+              if( collection.indexOf(dest) < 0 ) {
+                collection.push(dest);
+              }
 
-                obj[key] = mediaOut + dest;
+              obj[key] = mediaOut + dest;
 
-
-            }
-
+          }
         }
       }
 
@@ -66,7 +62,7 @@ module.exports = function(grunt) {
 
     }
 
-    function verifyDownload(dest, mediaOut, doneCallback) {
+    function verifyDownload( dest, mediaOut, doneCallback ) {
 
       var relativeToBucket = "";
 
@@ -74,23 +70,22 @@ module.exports = function(grunt) {
       dest = mediaOut + dest;
 
       //first check to see if the dest file exists in our project
-      fs.exists(dest, function(fileExists) {
+      fs.exists( dest, function( fileExists ) {
 
-        if (fileExists) {
-
+        if ( fileExists ) {
           //if the file exists, lets check to see if it needs to be re-downloaded.
           
-          client.headFile(relativeToBucket, function(err, res) {
+          client.headFile( relativeToBucket, function( err, res ) {
 
             var localHash = "";
             var remoteHash = res.headers.etag.replace(/"/g, '');
             //grab the remote etag from AWS, and compare it against a md5 of our local file.
 
-            fs.readFile(dest, function (err, data) {
+            fs.readFile( dest, function ( err, data ) {
 
               localHash = crypto.createHash('md5').update(data).digest('hex');
 
-              if (remoteHash === localHash) {
+              if ( remoteHash === localHash ) {
                 
                 //we don't need to download this file - its the same as what we've got.
                 doneCallback();
@@ -99,7 +94,7 @@ module.exports = function(grunt) {
               } else {
 
                 //the file hashes don't match, so we need to re-download it.
-                downloadFile(dest, relativeToBucket, doneCallback);
+                downloadFile( dest, relativeToBucket, doneCallback );
 
               }
 
@@ -149,19 +144,19 @@ module.exports = function(grunt) {
     function downloadFile(dest, src, doneCallback) {
 
       // Create a local stream we can write the downloaded file to.
-      grunt.log.ok("Grabbing down: " + dest);
+      grunt.log.ok("Downloading: " + dest);
 
       var file = fs.createWriteStream(dest);
       file.on("error", function(e) {
-        grunt.log.error("Error with filesystem creating write: " + dest);
-      })
+        grunt.log.error("Error creating file: " + dest);
+      });
 
       client.getFile(src, function (err, res) {
         // If there was an upload error or any status other than a 200, we
         // can assume something went wrong.
 
         if (err || res.statusCode !== 200) {
-          grunt.log.error("Error getting the file: " + dest);
+          grunt.log.error("Error retrieving file: " + dest);
           //return dfd.reject(makeError(MSG_ERR_DOWNLOAD, src, err || res.statusCode));
           doneCallback();
           return;
@@ -173,7 +168,7 @@ module.exports = function(grunt) {
           })
           .on('error', function (err) {
             //return dfd.reject(makeError(MSG_ERR_DOWNLOAD, src, err));
-            grunt.log.error("Hit an error thrown from AWS on file: " + dest);
+            grunt.log.error("AWS error for file: " + dest);
             doneCallback();
             return;
           })
@@ -230,56 +225,47 @@ module.exports = function(grunt) {
         grunt.log.error( "Error: " + body.message );
         return done( body.message );
       } else if (error) {
-        return done(error);
+        return done( error );
       } else if ((response.statusCode < 200 || response.statusCode > 399)) {
-        return done(response.statusCode + " " + body);
+        return done( "[" + response.statusCode + "] " + body );
       }
       
-      grunt.log.ok(response.statusCode);
+      if( options.out ) {
+        if( options.mediaOut !== "" ) {
 
-      if(options.out) {
-        
-
-        if(options.mediaOut != "") {
-
-            //if mediaOut doesn't end in "/", add it in.
-            if (options.mediaOut.substring(options.mediaOut.length-1) != "/") {
+            //if media doesn't end in "/", add it in.
+            if ( options.mediaOut.substring( options.mediaOut.length - 1 ) !== "/" ) {
                 options.mediaOut += "/";
             }
 
-            client = makeClient(options.privateConfig.aws);
-            mediaAssets = getMediaAssets(body, mediaAssets, options.mediaOut);
+            client = makeClient( options.aws );
+            mediaAssets = getMediaAssets( body, mediaAssets, options.mediaOut );
 
-            writeJsonFile(options.out, body);
+            writeJsonFile( options.out, body );
 
             var loadCounter = 0;
-            for(var key in mediaAssets) {
+            var next = function() {
+              loadCounter++;
+              if( loadCounter === mediaAssets.length ) {
+                done();
+              }
+            };
+
+            for( var key in mediaAssets ) {
                 var asset = mediaAssets[key];
-                verifyDownload(asset, options.mediaOut, function() {
-
-                  loadCounter++;
-                  if(loadCounter == mediaAssets.length) {
-        
-                    done();
-
-                  }
-
-                });
+                verifyDownload( asset, options.mediaOut, next );
             }
 
         } else {
 
-          writeJsonFile(options.out, body);
+          writeJsonFile( options.out, body );
           done();
 
         }
 
-        grunt.log.ok( "Content saved locally" );
+        grunt.log.ok( "Content saved locally." );
 
       }
-
     });
-
   });
-  
 };
